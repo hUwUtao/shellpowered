fdc89c986de6412aeab669e65d67af3d17e18ac0
huwutao <root@huwutao.me>
2023-07-06 23:25:10 +0700

    add date header?

--- utils/http.sh
+++ utils/http.sh
@@ -2,6 +2,7 @@ function header {
 cat <<EOF
 HTTP/1.1 $(grep "${STATUS:-"500"}" status.txt)
 Server: ncsh
+Date: $(date -Ru | etch "+")GMT
 Keep-Alive: timeout=5, max=1000
 Connection: Keep-Alive
 EOF
